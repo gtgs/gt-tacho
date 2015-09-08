d6a541861fa6c4706279eb46f7ffb5a54de1e304
Girish Sarwal <gsarwal@io-media.com>
2015-09-08 19:17:08 +0530

    fixed interrupts

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <avr/interrupt.h>
 #include "5110.h"
 
+#define F_CPU 8000000UL
 const char* sGt_Tacho = "GT-TACHOMETER";
 const char* sRevision= "1.0.0.0";
 
@@ -12,13 +13,22 @@ const char* sMax = "MAX %i";
 const char* sBlades = "Blades: %i";
 char* message = 0;
 volatile uint8_t blades = 2;
+volatile uint16_t sig = 0;
 volatile uint16_t rpm = 0;
 volatile uint16_t rps = 0;
 volatile uint16_t max = 0;
+ISR(INT0_vect)
+{
+	/** check for blades button pressed **/
+	if(++blades > 9){
+		blades = 2;
+	}
+	_delay_us(500);
+}
 void setup();
 int main(){
 	setup();			/** setup stuff **/
-	sei();				/** enable interrupts **/
+	sei();
 	lcd_clear();
 	lcd_goto_xy(0, 0);
 	lcd_str(sGt_Tacho);
@@ -28,39 +38,45 @@ int main(){
 	lcd_clear();
 	while(1){
 		/** print everything to the memory **/
+		rpm = sig/blades;
 		sprintf(message, sRpm, rpm);
 		lcd_goto_xy(0, 0);
 		lcd_str(message);
+
+		rps = rpm/60;
 
 		sprintf(message, sRps, rps);
 		lcd_goto_xy(0, 1);
 		lcd_str(message);
 
+
 		sprintf(message, sMax, max);
 		lcd_goto_xy(0, 2);
 		lcd_str(message);
 
+
 		sprintf(message, sBlades, blades);
 		lcd_goto_xy(0, 3);
 		lcd_str(message);
-
-		/** check for blades button pressed **/
-		if(1){//TODO: Replace with button pressed code
-			if(++blades > 9){
-				blades = 2;
-			}
-		}
+		sig+=100;
 	}
 	return 0;
 }
 
 void setup(){
-	/** Setup button input for blade change **/
-	/** setup button for light **/
-	/** setup pin change interrupt **/
+	DDRD |= (1 << PD0);	/** Setup button input for blade change **/
+	PORTD |= (1 << PD0);	/** Set Pull up **/
 	/** setup lcd **/
 	lcd_init(&PORTB, PB0, &PORTB, PB1, &PORTB, PB2, &PORTB, PB3, &PORTB, PB4);
 	/** Like, take a fixed byte size from memory for displaying messages **/
 	message = (char*) malloc(100);
+
+	MCUCR &= ~(1 << ISC00);
+	MCUCR |= (1 << ISC01);
+	GICR |= (1 << INT0);
+
+
+
+	//GIFR = (1 << INTF0) | (1 << INTF1);
 
 }
